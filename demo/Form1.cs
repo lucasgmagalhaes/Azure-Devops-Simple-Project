a995dfa654c458d1a24fe7125143394ba7911447
Lucas Gomes <lucasgsm88@gmail.com>
2019-05-27 19:54:04 -0300

    Corrigido build ?

---
+++ demo/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnsomar_Click(object sender, EventArgs e)
+        {
+            int numero1;
+            int numero2;
+
+            if (int.TryParse(txtn1.Text, out numero1) && int.TryParse(txtn2.Text, out numero2))
+            {
+                txtresultado.Text = (numero1 + numero2).ToString();
+            }
+            else
+            {
+                txtresultado.Text = "";
+            }
+        }
+    }
+}
